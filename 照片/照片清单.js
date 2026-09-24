9e47d7a890bad31bf9fbfdc5e490f2941de24a04
Chajie Zhou <u8042804@anu.edu.au>
2026-09-24 20:54:18 +1000

    Publish New Zealand South Island travel guide

---
+++ 照片/照片清单.js
@@ -0,0 +1,101 @@
+// 此文件由 tools/import-trip-photos.mjs 自动生成。
+window.GUIDE_PHOTO_ALBUMS = {
+  "DAY1/好牧羊人教堂": [
+    "DSC05360f.webp"
+  ],
+  "DAY1/拉凯亚峡谷": [
+    "DSC05228f.webp",
+    "IMG_2595.webp"
+  ],
+  "DAY10": [
+    "基督城市区涂鸦.webp",
+    "基督城市区晚餐.webp",
+    "Timaru中途午餐.webp"
+  ],
+  "DAY11": [
+    "悬浮房子.webp"
+  ],
+  "DAY2/80号公路库克山拍摄点Peter's Lookout": [
+    "IMG_2665.webp",
+    "IMG_2809.webp",
+    "IMG_4320f.webp"
+  ],
+  "DAY2/Blue Lakes": [
+    "IMG_2758.webp"
+  ],
+  "DAY2/Tasman Glacier": [
+    "IMG_2734.webp"
+  ],
+  "DAY2/库克山": [
+    "IMG_2708.webp"
+  ],
+  "DAY2/库克山前公路": [
+    "IMG_2700.webp"
+  ],
+  "DAY2/普卡基湖彼得观景点": [
+    "IMG_2645.webp"
+  ],
+  "DAY3/欧玛拉玛粘土峭壁": [
+    "IMG_2863.webp",
+    "IMG_2867.webp"
+  ],
+  "DAY3/瓦纳卡孤独之树": [
+    "IMG_3027.webp",
+    "IMG_3289.webp"
+  ],
+  "DAY4/realgun": [
+    "IMG_1294.webp"
+  ],
+  "DAY4/皇后镇 Skyline 天际缆车": [
+    "IMG_4322f.webp"
+  ],
+  "DAY4/箭镇": [
+    "IMG_3133.webp"
+  ],
+  "DAY5/格林诺奇雨夜": [
+    "IMG_3397.webp",
+    "IMG_3463f.webp"
+  ],
+  "DAY5/雨天皇后镇": [
+    "IMG_3358.webp"
+  ],
+  "DAY6": [
+    "蒂阿瑙湖前小酌.webp",
+    "缆车上自助餐厅景色.webp",
+    "路边小店.webp",
+    "民宿风景.webp"
+  ],
+  "DAY7": [
+    "1855观景台f.webp",
+    "返程路上遇到的啄羊鹦鹉1f.webp",
+    "返程路上遇到的啄羊鹦鹉2f.webp",
+    "峡湾.webp",
+    "巡游瀑布.webp"
+  ],
+  "DAY8": [
+    "海景泡澡.webp",
+    "海景住宿.webp",
+    "沙滩上的海狮.webp",
+    "沙滩上的海狮2f.webp",
+    "沿途丁达尔效应.webp",
+    "夜间泡澡.webp",
+    "SandflyBayBeach.webp"
+  ],
+  "DAY9": [
+    "鲍德温大街.webp",
+    "海豹宝宝1.webp",
+    "海豹宝宝2.webp"
+  ],
+  "皇后镇穷鬼美食推荐": [
+    "大脸汉堡.webp",
+    "大脸汉堡甜甜圈.webp",
+    "大脸汉堡GELATO.webp",
+    "flame黑虎虾汤饭.webp",
+    "flame猪排.webp",
+    "Patagonia1.webp",
+    "Patagonia2.webp",
+    "SaigonKingdom.webp",
+    "tacoLand.webp",
+    "WindyRidgeMilk.webp"
+  ]
+};
---
+++ 照片/照片配置.js
@@ -0,0 +1,6 @@
+window.GUIDE_PHOTO_CONFIG = {
+  friendCredit: "方块",
+  friendWatermark: "小红书799827199 方块",
+  defaultCredit: "茶",
+  defaultWatermark: "小红书958614780 茶"
+};
